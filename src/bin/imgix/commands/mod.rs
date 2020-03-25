8c3a6a69e9f63a1fa8cf4f30e2029aa5030e1eae
Eric Sanchez <esanchez@imgix.com>
2020-03-25 01:21:25 -0700

    Implements the `pre-commit` cli command

--- src/bin/imgix/commands/mod.rs
+++ src/bin/imgix/commands/mod.rs
@@ -1,7 +1,11 @@
 use imgix::command_prelude::*;
 
+/// This function exists to export the cli configuration for all sub
+/// commands. It works by populating a `Vec` with clap `App`s. Each
+/// `App` defines the command line interface (cli) for _it's module_.
 pub fn all_sub_commands() -> Vec<App> {
-    vec![init::cli()]
+    vec![init::cli(), pre_commit::cli()]
 }
 
 pub mod init;
+pub mod pre_commit;
